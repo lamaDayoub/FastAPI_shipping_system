f1029599cef11e6ac7471ac3606e1e7f002db4f4
lamaDayoub <lama1e2dayoub@gmail.com>
2026-02-23 02:19:28 +0300

    have a database class with all methods to use with the apis

--- app/database.py
+++ app/database.py
@@ -1,49 +1,80 @@
+from multiprocessing import connection
 import sqlite3
+from schemas import ShipmentCreate,ShipmentUpdate
+from typing import Any
+class Database():
+    def __init__(self):
+        #Make the Connection
+        self.conn = sqlite3.connect('sqlit.db')
+        #curdor to excute the sql lanaguage commands
+        self.cur =self. conn.cursor()
+        self.create_tabe('shipment')
+                
+    def create_tabe(self,name:str):
+        self.cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS ?(
+                id INT PRIMARY KEY,
+                content TEXT,
+                weight REAL,
+                status TEXT
+            )
+            """,(name,)
+        )
+        
+    def create(self, shipment : ShipmentCreate):
+        self.cur.execute("SELECT MAX(id) FROM shipment")
+        result = self.cur.fetchone()
+        new_id = result[0] +1
+        self.cur.execute("""
+            INSERT INTO shipment VALUES(
+                :id, :content , :weight , :status
+            )
+        """,
+            {
+                "id":new_id,
+                **shipment.model_dump(),
+                "status":'placed'
+            }
+        )
+        self.conn.commit()
+        return new_id
+        
+    def get(self, id:int) -> dict[str :Any] |None:
+        self.cur.execute("""
+        SELECT * FROM shipment  WHERE id=?                          
+        """,(id,))
+        row = self.cur.fetchone()
+        return{
+            "id":row[0],
+            "content":row[1],
+            "weight":row[2],
+            "status":row[3]
+        } if row else None
 
-#Make the Connection
-connection = sqlite3.connect('sqlit.db')
+    def update(self, shipment:ShipmentUpdate)-> dict[str,Any]:
+        
+        self.cur.execute("""
+            UPDATE shipment SET status = :status
+            WHERE id = :id
+            """,
+                {
+                    "id":id,
+                    **shipment.model_dump()
+                    
+                }
+            )
+        self.conn.commit()
+        return self.get(id)
+        
+    def delete(self , id:int):
+        self.cur.execute("DELETE FROM shipment WHERE id = ?",(id,))
+        self.conn.commit()
+        
+    def close(self):
+        self.conn.close()
 
 
-#curdor to excute the sql lanaguage commands
-cursor = connection.cursor()
-#1creating a table
-cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS shipment(
-        id INT PRIMARY KEY,
-        content TEXT,
-        weight REAL,
-        status TEXT
-    )
-    """
-)
-# adding a row
-# cursor.execute("""
-#     INSERT INTO shipment VALUES(
-#         12701,'desktop',10,'placed'
-#     )
-# """)
-# connection.commit()
-#updating
-cursor.execute("""
-        UPDATE shipment SET status = 'delivered'
-        WHERE id = 12701
-               """)
-connection.commit()
-#  selecting from the table
 
-# cursor.execute("""
-#     SELECT * FROM shipment                            
-# """)
-# result = cursor.fetchmany(2)
-# print(result)
 
-# cursor.execute("""
-#     SELECT * FROM shipment  WHERE id=12701                          
-# """)
-# result2 = cursor.fetchone()
-# print(result2)
-# delete the table
-# cursor.execute("DROP TABLE shipment")
-# connection.commit()
-connection.close()
+
