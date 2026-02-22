afbdd0eb6a1b3d86c1fe138bcb2bacad4f88bc34
lamaDayoub <lama1e2dayoub@gmail.com>
2026-02-22 17:33:57 +0300

    connecting to sqlite and make a table ,insert a row

--- app/database.py
+++ app/database.py
@@ -1,24 +1,27 @@
-import json
-from pathlib import Path
+import sqlite3
 
-# Get the directory where database.py actually lives
-BASE_DIR = Path(__file__).resolve().parent.parent 
-# Join the directory with the filename
-json_path = BASE_DIR / "shipments.json"
-shipments ={}
-try:
-    with open(json_path) as json_file:
-        data = json.load(json_file)
-        for value in data:
-            shipments[value['id']] = value
-except FileNotFoundError:
-    print(f"Warning: {json_path} not found. Starting with empty shipments.")
+#Make the Connection
+connection = sqlite3.connect('sqlit.db')
 
-print(shipments)
 
-def save():
-    with open(json_path ,'w')  as json_file:
-        json.dump(
-           list( shipments.values()),
-           json_file
-        )
+#curdor to excute the sql lanaguage commands
+cursor = connection.cursor()
+
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS shipment(
+        id INT,
+        content TEXT,
+        weight REAL,
+        status TEXT
+    )
+    """
+)
+cursor.execute("""
+    INSERT INTO shipment VALUES(
+        12702,'desktop',10,'placed'
+    )
+""")
+connection.commit()
+#Close the connection when done
+connection.close()
