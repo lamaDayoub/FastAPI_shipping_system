ce6c3d633b454ed1957419affb9b185c60b2c6a2
lamaDayoub <lama1e2dayoub@gmail.com>
2026-02-23 00:45:32 +0300

    adding a primary key and edit a row

--- app/database.py
+++ app/database.py
@@ -6,26 +6,31 @@ connection = sqlite3.connect('sqlit.db')
 
 #curdor to excute the sql lanaguage commands
 cursor = connection.cursor()
-#1 creating a table
-# cursor.execute(
-#     """
-#     CREATE TABLE IF NOT EXISTS shipment(
-#         id INT,
-#         content TEXT,
-#         weight REAL,
-#         status TEXT
+#1creating a table
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS shipment(
+        id INT PRIMARY KEY,
+        content TEXT,
+        weight REAL,
+        status TEXT
+    )
+    """
+)
+# adding a row
+# cursor.execute("""
+#     INSERT INTO shipment VALUES(
+#         12701,'desktop',10,'placed'
 #     )
-#     """
-# )
-# #2 adding a row
-# # cursor.execute("""
-# #     INSERT INTO shipment VALUES(
-# #         12702,'desktop',10,'placed'
-# #     )
-# # """)
-# # connection.commit()
-
-# #3 selecting from the table
+# """)
+# connection.commit()
+#updating
+cursor.execute("""
+        UPDATE shipment SET status = 'delivered'
+        WHERE id = 12701
+               """)
+connection.commit()
+#  selecting from the table
 
 # cursor.execute("""
 #     SELECT * FROM shipment                            
@@ -38,7 +43,7 @@ cursor = connection.cursor()
 # """)
 # result2 = cursor.fetchone()
 # print(result2)
-# #Close the connection when done
-cursor.execute("DROP TABLE shipment")
-connection.commit()
+# delete the table
+# cursor.execute("DROP TABLE shipment")
+# connection.commit()
 connection.close()
