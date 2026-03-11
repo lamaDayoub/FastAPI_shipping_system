5e4b81bfcf8f9a8bc1093e8c5ce6c82f0f2261fb
lamaDayoub <lama1e2dayoub@gmail.com>
2026-03-11 16:48:11 +0300

    add a delivery_partner table and migrate with alembic

--- app/database/models.py
+++ app/database/models.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 from enum import Enum
+from typing import List
 from pydantic import EmailStr
 from sqlalchemy import Column
 from sqlmodel import Relationship, SQLModel,Field
 from uuid import uuid4,UUID
 from sqlalchemy.dialects import postgresql  
-
+from sqlalchemy import ARRAY, INTEGER
 class ShipmentStatus(str, Enum):
     placed = "placed"
     in_transit = "in_transit"
     out_for_delivery = "out_for_delivery"
     delivered = "delivered"
 
-class Shipment (SQLModel, table =True):
+class Shipment (SQLModel, table =True): 
     __tablename__='shipment'
     
     id: UUID = Field(
@@ -28,12 +29,29 @@ class Shipment (SQLModel, table =True):
     status: ShipmentStatus
     estimated_delivery: datetime
     seller_id : UUID = Field(foreign_key = "seller.id")
+    created_at: datetime = Field(
+        sa_column= Column(
+            postgresql.TIMESTAMP,
+            default = datetime.now,
+        )
+    )
     seller : "Seller" = Relationship(
         back_populates = "shipments",
         sa_relationship_kwargs={"lazy":"selectin"}
     )
+    delivery_partner_id : UUID =Field(foreign_key = "delivery_partner.id")
+    delivery_partner : "DeliveryPartner" = Relationship(
+        back_populates="shipments",
+        sa_relationship_kwargs={"lazy":"selectin"}
+    )
     
-class Seller(SQLModel, table= True):
+class User(SQLModel):
+    name:str
+    email:EmailStr
+    password_hash: str
+    
+class Seller(User, table= True):
+    __tablename__='seller'
     id: UUID = Field(
         sa_column = Column(
             postgresql.UUID(),
@@ -41,11 +59,38 @@ class Seller(SQLModel, table= True):
             primary_key = True,
         )
     )
-    name:str
-    email:EmailStr
-    password_hash: str
+    created_at: datetime = Field(
+        sa_column= Column(
+            postgresql.TIMESTAMP,
+            default = datetime.now,
+        )
+    )
     shipments:list[Shipment]=Relationship(
         back_populates ="seller",
         sa_relationship_kwargs={"lazy":"selectin"}
     )
     address: int
+    
+class DeliveryPartner(User, table= True):
+    __tablename__='delivery_partner'
+    id: UUID = Field(
+        sa_column = Column(
+            postgresql.UUID(),
+            default = uuid4,
+            primary_key = True,
+        )
+    )   
+    serviceable_zib_codes : List[int] = Field(
+        sa_column = Column(postgresql.ARRAY(INTEGER))
+    )
+    created_at: datetime = Field(
+        sa_column= Column(
+            postgresql.TIMESTAMP,
+            default = datetime.now,
+        )
+    )
+    max_handling_capacity : int
+    shipments : "Shipment" = Relationship(
+        back_populates="delivery_partner",
+        sa_relationship_kwargs={"lazy":"selectin"}
+    )
--- migrations/env.py
+++ migrations/env.py
@@ -6,13 +6,17 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from app.config import db_settings
 from sqlmodel import SQLModel
-from app.database.models import Seller, Shipment
+from app.database.models import Shipment,Seller,DeliveryPartner
+from app.config import db_settings
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', db_settings.POSTGRES_URL)
+config.set_main_option(
+    "sqlalchemy.url", db_settings.POSTGRES_URL
+    
+)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
